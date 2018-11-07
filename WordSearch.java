f79f45f19832c302bb426bf7ab7561f8f080925a
weichanhyao <wyao1@stuy.edu>
2018-11-07 18:15:49 -0500

    made the basic methods

---
+++ WordSearch.java
@@ -0,0 +1,56 @@
+public class WordSearch{
+  private char[][]data;
+  public WordSearch(int rows,int cols){
+    char[][]data = new char[rows][cols];
+    for (int i = 0; i < rows; i++){
+      for (int j = 0; j < cols; j++){
+        data[i][j] = '_';
+      }
+    }
+  }
+  private void clear(){
+    for (int i = 0; i < data.length; i++){
+      for (int j = 0; j < data[i].length; j++){
+        data[i][j] = '_';
+      }
+    }
+  }
+  public String toString(){
+    String twoD = "";
+    for (int i = 0; i < data.length; i++){
+      for (int j = 0; j < data[i].length; j++){
+        twoD = twoD + data[i][j] + " ";
+      }
+      twoD = twoD + "\n";
+    }
+    return twoD;
+  }
+  //I think I need to make this thing -1 cuz length is one more than
+  //the last index!!!!
+  public boolean addWordHorizontal(String word,int row, int col){
+    if ((col + word.length()) > data[row].length){
+      return false;
+    }
+    for(int i = 0; i<word.length(); i++){
+      if ((data[row][i+col] == '_') || (word.charAt(i) == data[row][i+col])){
+        data[row][i+col] = word.charAt(i);
+      }
+    }
+    return true;
+  }
+  public boolean addWordVertical(String word,int row, int col){
+    if ((row + word.length()) > data.length){
+      return false;
+    }
+    for(int i = 0; i<word.length(); i++){
+      if ((data[row+i][col] == '_') || (word.charAt(i) == data[row+i][col])){
+        data[row+i][col] = word.charAt(i);
+      }
+    }
+    return true;
+  }
+}
+
+        
+  
+    
