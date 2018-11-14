677c112ef12dbebcd13ae4b7569e7d9bcb12a2e9
weichanhyao <wyao1@stuy.edu>
2018-11-14 01:53:03 -0500

    added addWord. Didn't test it tho

--- WordSearch.java
+++ WordSearch.java
@@ -1,9 +1,13 @@
 //stuff to do
 //do i need to make sure the stuff fits? like (-1,600) wont work
 //toString - add the bottom stuff (add words that were added to a list and then print the list out?)
-
-
+import java.util.ArrayList;
+import java.util.Random;
 public class WordSearch{
+  private int seed;
+  private Random randgen;
+  private ArrayList<String>wordsToAdd;
+  private ArrayList<String>wordsAdded;
   private char[][] data;
   public WordSearch(int rows,int cols){
     data = new char[rows][cols];
@@ -34,6 +38,10 @@ public class WordSearch{
         twoD += "|";
       }
     }
+    //twoD += "\nWords:";
+    //for (int i = 0; i < wordsAdded.size(); i++){
+    //  twoD += wordsAdded.get(i);
+    //}
     return twoD;
   }
 
@@ -84,19 +92,27 @@ public class WordSearch{
         return false;
       }
     }
-     for (int i = 0; i < word.length(); i ++){
+    for (int i = 0; i < word.length(); i ++){
       data[row + i][col + i] = word.charAt(i);
     }
     return true;
   }
-  //private boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
-  //still have to add whether it fits or not
-  /**for (int i = 0; i < word.length(); i ++){
-    *  if (Character.isLetter(data[i * rowIncrement + row][i * colIncrement + col]) && (! (data[i * rowIncrement + row][i * colIncrement + col] == word.charAt(i)))){
-    *    return false;
-    *  }
-    *}//I know this part is good
-    */
+  private boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
+    if (word.length() + row * rowIncrement > data.length || word.length() + col * colIncrement > data[0].length || word.length() + row * rowIncrement < 0 || word.length() + col * colIncrement < 0){
+      return false;
+    }
+    for (int i = 0; i < word.length(); i ++){
+      if (Character.isLetter(data[i * rowIncrement + row][i * colIncrement + col]) && (! (data[i * rowIncrement + row][i * colIncrement + col] == word.charAt(i)))){
+        return false;
+      }
+    }//I know this part is good
+    for(int i = 0; i<word.length(); i++){
+      if ((data[row+i*rowIncrement][col+i*colIncrement] == '_') || (word.charAt(i) == data[row+i*rowIncrement][col+i*colIncrement])){
+        data[row+i*rowIncrement][col+i*colIncrement] = word.charAt(i);
+      }
+    }
+    return true;
+  }  
 }
 
         
